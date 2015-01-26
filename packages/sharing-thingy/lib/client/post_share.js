ab4c0dac39d9ea6397a43ea6ab2773da921812e7
msintaha <msintaha94@gmail.com>
2015-01-26 15:53:50 +0600

    Update post_share.js

--- packages/sharing-thingy/lib/client/post_share.js
+++ packages/sharing-thingy/lib/client/post_share.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
   Template[getTemplate('postShare')].helpers({
     sourceLink: function(){
-      return !!this.url ? this.url : getSiteUrl() + "posts/"+this._id;
+      return !!this.url ? this.url : "http://make-a-difference.rahimafrooz.com/" + "posts/"+this._id;
     },
     viaTwitter: function () {
       return !!getSetting('twitterAccount') ? 'via='+getSetting('twitterAccount') : '';
